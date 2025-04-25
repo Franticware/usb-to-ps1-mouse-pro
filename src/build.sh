aabeba74bf9996dd19663555d43a18358efcdb70
Vojtěch Salajka <vojta.salajka@seznam.cz>
2025-04-25 23:54:22 +0200

    Start over with updated libraries.

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#export PICO_SDK_FETCH_FROM_GIT=1
+export PICO_SDK_PATH=../pico-sdk
+
 mkdir build
 cd build
 cmake ..
 make
-cd ..
---
+++ src/get_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone --branch 0.7.1 --single-branch https://github.com/sekigon-gonnoc/Pico-PIO-USB.git
+git clone --branch 2.1.1 --recursive --single-branch https://github.com/raspberrypi/pico-sdk.git
