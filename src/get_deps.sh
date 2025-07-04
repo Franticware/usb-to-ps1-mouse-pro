2f312328317ea6502ca8c0bc9dadad831ef496d2
Vojtěch Salajka <vojta.salajka@seznam.cz>
2025-07-04 23:41:45 +0200

    Add pico-sdk hotfix.

--- src/get_deps.sh
+++ src/get_deps.sh
@@ -2,3 +2,8 @@
 
 git clone --branch 0.7.1 --single-branch https://github.com/sekigon-gonnoc/Pico-PIO-USB.git
 git clone --branch 2.1.1 --recursive --single-branch https://github.com/raspberrypi/pico-sdk.git
+
+# pico-sdk hotfix
+sed -i '1i#include <cstdint>' pico-sdk/tools/pioasm/pio_assembler.h
+sed -i '1i#include <cstdint>' pico-sdk/tools/pioasm/output_format.h
+sed -i '1i#include <cstdint>' pico-sdk/tools/pioasm/pio_types.h
